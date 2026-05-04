4637d88bf53498aa15f3b6c2347e10d1ff6ab1d3
merazhossain5662-hash <merazhossain5662@gmail.com>
2026-05-04 15:21:00 +0600

    added global loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div className="w-10  mx-auto">
+            <span className="loading loading-infinity mx-auto loading-xl  text-accent"></span>
+        </div>
+    );
+};
+
+export default loading;
